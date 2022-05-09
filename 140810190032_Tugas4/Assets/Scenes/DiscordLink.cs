f3a4efb7c910de13c47428e26a2ca7728373e2c6
akirareka <akira.jiraiya@gmail.com>
2022-05-09 09:44:37 +0700

    commit update
    commit update

---
+++ 140810190032_Tugas4/Assets/Scenes/DiscordLink.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiscordLink : MonoBehaviour
+{
+    public void OpenChannel()
+    {
+        Application.OpenURL("https://discord.gg/Kxzr8wqN");
+    }
+}
