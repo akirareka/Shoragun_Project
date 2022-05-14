80801e40eb402cdb4c3f2db5a757ba6a46062ec8
akirareka <akira.jiraiya@gmail.com>
2022-05-14 22:24:18 +0700

    update progress

---
+++ 140810190032_Tugas4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5f;
+    [SerializeField]
+    private float lookSensitivity = 3f;
+    [SerializeField]
+    private float thrusterForce = 1000f;
+
+    private PlayerMotor motor;
+
+    void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    void Update()
+    {
+        float _xMov = Input.GetAxisRaw("Horizontal");
+        float _zMov = Input.GetAxisRaw("Vertical");
+
+        Vector3 _movHorizontal = transform.right * _xMov;
+        Vector3 _movVertical = transform.forward * _zMov;
+
+        //Final movement vector
+        Vector3 _velocity = (_movHorizontal + _movVertical).normalized * speed;
+        motor.Move(_velocity);
+
+
+        //Calculate rotation as a 3D Vector (turning around)
+        float _yRot = Input.GetAxis("Mouse X");
+
+        Vector3 _rotation = new Vector3(0f, _yRot, 0f) * lookSensitivity;
+
+        //Apply rotation
+        motor.Rotate(_rotation);
+
+        //Calculate camera rotation as a 3D Vector (turning around)
+        float _xRot = Input.GetAxis("Mouse Y");
+
+        Vector3 _cameraRotation = new Vector3(_xRot, 0f, 0f) * lookSensitivity;
+
+        //Apply camera rotation
+        motor.RotateCamera(_cameraRotation);
+
+        //Calculate thruster force input
+        Vector3 _thrusterForce = Vector3.zero;
+        if (Input.GetButton("Jump"))
+        {
+            _thrusterForce = Vector3.up * thrusterForce;
+        }
+
+        //Apply thruster force
+        motor.ApplyThruster(_thrusterForce);
+    }
+}
